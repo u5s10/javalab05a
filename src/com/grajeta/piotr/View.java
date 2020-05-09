c0e9bc635740e789b8b911e38bbcea12c64f781d
grajetapiotr@gmail.com <grajetapiotr@gmail.com>
2020-05-09 18:31:07 +0200

    done

--- src/com/grajeta/piotr/View.java
+++ src/com/grajeta/piotr/View.java
@@ -13,92 +13,116 @@ public class View {
         this.controller = controller;
     }
 
-    public void run(){
+    public void run() {
         Scanner scanner = new Scanner(System.in);
-        String inputString;
+        String inputString = "0";
         int columns;
         int rows;
         this.promptMenu();
-        while (scanner.hasNextLine()) {
-            this.promptMenu();
+        label:
+        while (true) {
             inputString = scanner.nextLine();
-            if (inputString.equals("1")) {
-                System.out.println("Podaj liczbe wierszy");
-                rows = scanner.nextInt();
-                System.out.println("Podaj liczbe kolumn");
-                columns = scanner.nextInt();
-                controller.createMatrices(rows, columns);
-                System.out.println("Wprowadz wartosci do macierzy A.");
-                int temp = 0;
-                for (int i = 0; i < rows; i++) {
-                    for (int j = 0; j < columns; j++) {
-                        temp = scanner.nextInt();
-                        controller.populateMatrixA(i, j, temp);
+            switch (inputString) {
+                case "1":
+                    System.out.println("Podaj liczbe wierszy");
+                    check(scanner);
+                    rows = scanner.nextInt();
+                    System.out.println("Podaj liczbe kolumn");
+                    check(scanner);
+                    columns = scanner.nextInt();
+                    controller.createMatrices(rows, columns);
+                    System.out.println("Wprowadz wartosci do macierzy A.");
+                    int temp;
+                    for (int i = 0; i < rows; i++) {
+                        for (int j = 0; j < columns; j++) {
+                            check(scanner);
+                            temp = scanner.nextInt();
+                            controller.populateMatrixA(i, j, temp);
+                        }
                     }
-                }
-                System.out.println("Wprowadz wartosci do macierzy B.");
-                for (int i = 0; i < columns; i++) {
-                    for (int j = 0; j < rows; j++) {
-                        temp = scanner.nextInt();
-                        controller.populateMatrixB(i, j, temp);
+                    System.out.println("Wprowadz wartosci do macierzy B.");
+                    for (int i = 0; i < columns; i++) {
+                        for (int j = 0; j < rows; j++) {
+                            check(scanner);
+                            temp = scanner.nextInt();
+                            controller.populateMatrixB(i, j, temp);
+                        }
                     }
-                }
 
-            } else if (inputString.equals("2")) {
-                this.promtpOperations();
-                while(scanner.hasNextLine()){
+                    break;
+                case "2":
                     this.promtpOperations();
                     inputString = scanner.nextLine();
-                    if(inputString.equals("1")){
-                        controller.transponeInputMatrices();
-                    }else if(inputString.equals("2")){
-                        // multiplication
-                    }else if(inputString.equals("3")){
-                        controller.transponeOutputMatix();
-                    }else
-                        break;
-                }
+                    if (inputString.equals("1")) {
+                        if (controller.getModel().getMatrixA() == null) {
+                            System.err.println("Brak macierzy A i B");
+                            continue;
+                        } else
+                            controller.transponeInputMatrices();
+                    } else if (inputString.equals("2")) {
+                        if (controller.getModel().getMatrixA() == null) {
+                            System.err.println("Brak macierzy A i B");
+                            continue;
+                        } else
+                            controller.calculateMatrixC();
+                    } else if (inputString.equals("3")) {
+                        if (controller.getModel().getMatrixC() == null) {
+                            System.err.println("Brak macierzy C");
+                            continue;
+                        } else
+                            controller.transponeOutputMatix();
+                    }
+                    this.promptMenu();
 
-
-            } else if (inputString.equals("3")) {
-                this.promtpResults();
-                while(scanner.hasNextLine()) {
+                    break;
+                case "3":
                     this.promtpResults();
                     inputString = scanner.nextLine();
                     if (inputString.equals("1")) {
                         this.showMatricesAB();
                     } else if (inputString.equals("2")) {
-                        controller.calculateMatrixC();
-                        this.showMatrixC();
-                    } else {
-                        break;
+                        if (controller.getModel().getMatrixC() == null) {
+                            System.err.println("Brak macierzy C");
+                        } else
+                            this.showMatrixC();
                     }
-                }
+                    this.promptMenu();
 
-            } else if (inputString.equals("q")) {
-                break;
+                    break;
+                case "q":
+                    break label;
+                default:
+                    this.promptMenu();
+                    break;
             }
         }
         scanner.close();
 
     }
 
+    private static void check(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String input = scanner.next();
+            System.out.printf("\"%s\" nie jest liczba calkowita.\n", input);
+        }
+    }
+
     public void promptMenu() {
 
         System.out.println(
-                "1.Wprowadz macierze wejsciowe\n2.Operacje arytmetyczne\n3.Wyswietl macierze");
+                "1.Wprowadz macierze wejsciowe\n2.Operacje arytmetyczne\n3.Wyswietl macierze\n");
     }
 
     public void promtpOperations() {
 
         System.out.println(
-                "1.Transponuj macierze\n2.Pomnoz macierze wejsciowe\n3.Transponuj macierz wynikowa");
+                "1.Transponuj macierze\n2.Pomnoz macierze wejsciowe\n3.Transponuj macierz wynikowa\n");
     }
 
     public void promtpResults() {
 
         System.out.println(
-                "1.Wyswietl macierze wejsciowe\n2.Wyswielt macierz wynikowa");
+                "1.Wyswietl macierze wejsciowe\n2.Wyswielt macierz wynikowa\n");
     }
 
     public void showMatricesAB() {
@@ -123,9 +147,10 @@ public class View {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
-    public void showMatrixC(){
+    public void showMatrixC() {
         int[][] matrixC = this.controller.getModel().getMatrixC();
         int rows = this.controller.getModel().getMatrixC().length;
         int columns = this.controller.getModel().matrixC[0].length;
@@ -137,6 +162,7 @@ public class View {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
 }
